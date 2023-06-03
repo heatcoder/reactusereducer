82f724a48de9cc2632dc1dc202fd8a32d37d6192
Heatcoder <v_rajiv24@yahoo.com>
2023-06-03 21:40:59 +0800

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  import './App.css';
+  import { useReducer } from 'react';
 
-export default App;
+  const initialState = {counter:0}
+  function App() {
+
+    const reducer=(prevState, action)=>{
+      console.log(prevState)
+    switch(action.type) {
+
+      case "INCREMENT": 
+      
+      return {counter:prevState.counter + action.payload}
+      
+      case "DECREMENT" : 
+      if (prevState.counter >0) {
+        return {counter:prevState.counter - action.payload}
+      } else {
+        return prevState
+      }
+      
+      
+      default: 
+      return {prevState}
+
+    }
+    }
+
+    const [state, dispatch ]=useReducer(reducer, initialState)
+
+    return (
+      <>
+      <h1>{state.counter}</h1>
+    <button onClick={()=>dispatch( {type:"INCREMENT", payload: 2})}> Increase </button>
+    <button onClick={()=>dispatch( {type:"DECREMENT", payload: 2})}> Decrease </button>
+
+      </>
+
+    );
+  }
+
+  export default App;
